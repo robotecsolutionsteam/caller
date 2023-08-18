575a542bf2cb740b74bcf605a42937f9f86906cf
Nicolasalan <nicolasalangrotti03@gmail.com>
2023-08-18 14:04:15 -0300

    init commit

--- temi/src/main/java/com/robotec/temi/HelloWorld.kt
+++ temi/src/main/java/com/robotec/temi/HelloWorld.kt
@@ -4,14 +4,7 @@ import android.content.Context
 import android.widget.Toast
 
 object Hello {
-    fun sayHello() {
-        println("Hello, world!")
-    }
     fun showToast(context: Context?, message: String?) {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
-    }
-    fun Sum(var1: Int, var2: Int) {
-        val sum = var1 + var2
-        return sum
     }
 }
