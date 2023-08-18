74a803d8b07544b172300e06871dbfa86739f3ef
Nicolasalan <nicolasalangrotti03@gmail.com>
2023-08-18 16:40:46 -0300

    update

--- app/src/main/java/com/robotec/caller/MainActivity.kt
+++ app/src/main/java/com/robotec/caller/MainActivity.kt
@@ -1,25 +1,51 @@
 package com.robotec.caller
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
+import com.robotec.caller.databinding.ActivityMainBinding
 import com.robotec.temi.Commands
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), View.OnClickListener{
 
-    // instance of Commands class
+    private lateinit var binding: ActivityMainBinding
+
     private val commands = Commands()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        // call goto() method from Commands class
-        commands.goto("sofa")
-
-        commands.angle(90)
-        commands.turnBy(60)
-        commands.follow()
-        commands.speech("Bora familia")
-        commands.unfollow()
+        // Acesso aos elementos de interface via 'binding'
+        binding.buttonSpeak.setOnClickListener(this)
+        binding.buttonGoto.setOnClickListener(this)
+        binding.buttonFollow.setOnClickListener(this)
+        binding.buttonTurn.setOnClickListener(this)
+        binding.buttonAngle.setOnClickListener(this)
     }
-}
+
+    override fun onClick(view: View) {
+
+            when (view.id) {
+                R.id.button_speak -> {
+                    commands.speech("Bora familia")
+                }
+                R.id.button_goto -> {
+                    commands.goto("sofa")
+                }
+                R.id.button_follow -> {
+                    commands.follow()
+                }
+                R.id.button_angle -> {
+                    commands.angle(90)
+                }
+                R.id.button_turn -> {
+                    commands.turnBy(60)
+                }
+                R.id.button_unfollow -> {
+                    commands.unfollow()
+                }
+            }
+        }
+    }
--- temi/src/main/java/com/robotec/temi/Listener.kt
+++
@@ -1,4 +0,0 @@
-package com.robotec.temi
-
-class Listener {
-}
