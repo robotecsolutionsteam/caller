88e77fc7f3cb0496aa79048413c636484faef312
Nicolasalan <nicolasalangrotti03@gmail.com>
2024-02-20 10:27:58 -0300

    Adicionado funcoes de deteccao

--- caller/src/main/java/com/robotec/caller/vision/Detection.kt
+++ caller/src/main/java/com/robotec/caller/vision/Detection.kt
@@ -3,9 +3,10 @@ package com.robotec.caller.vision
 import android.util.Log
 import com.robotec.caller.utils.Request
 import com.robotemi.sdk.Robot
-import com.robotemi.sdk.TtsRequest
+import com.robotemi.sdk.listeners.OnDetectionDataChangedListener
 import com.robotemi.sdk.listeners.OnDetectionStateChangedListener
-import com.robotemi.sdk.listeners.OnGoToLocationStatusChangedListener
+import com.robotemi.sdk.listeners.OnUserInteractionChangedListener
+import com.robotemi.sdk.model.DetectionData
 
 class Detection {
 
@@ -37,22 +38,53 @@ class Detection {
         } catch (e: Exception) {
             Log.e("TemiCaller", "Erro ao reconhecer")
         }
-
     }
 
-    fun trackerOn(distance: Float) {
-        request.requestTrackUser()
-
+    fun trackerOn() {
         try {
-            temiRobot.trackUserOn
+            request.requestTrackUser()
         } catch (e: Exception) {
             Log.e("TemiCaller", "Erro ao reconhecer")
         }
 
     }
 
-    fun interaction() {
-        
+    fun interaction(onComplete: () -> Unit) {
+        try {
+
+            val interactionStatus = object : OnUserInteractionChangedListener {
+                override fun onUserInteraction(
+                    isInteracting: Boolean
+                ) {
+                    if (isInteracting) {
+                        temiRobot.removeOnUserInteractionChangedListener(this)
+                        onComplete.invoke()
+
+                    }
+                }
+            }
+            temiRobot.addOnUserInteractionChangedListener(interactionStatus)
+        } catch (e: Exception) {
+            Log.e("TemiCaller", "Erro ao reconhecer interacao")
+        }
+    }
+
+    fun dataDetection(onComplete: () -> Unit) {
+        try {
+
+            val dataStatus = object : OnDetectionDataChangedListener {
+                override fun onDetectionDataChanged(
+                    detectionData: DetectionData
+                ) {
+                    // TODO: fazer retornar a resposta
+                    temiRobot.removeOnDetectionDataChangedListener(this)
+                    onComplete.invoke()
+                }
+            }
+            temiRobot.addOnDetectionDataChangedListener(dataStatus)
+        } catch (e: Exception) {
+            Log.e("TemiCaller", "Erro ao reconhecer")
+        }
     }
 
 }
